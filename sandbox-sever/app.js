8a946df8d9e5b3dc325be5300497ed22f15b945f
yinon <89408487+yinonperets@users.noreply.github.com>
2022-10-23 22:58:06 +0300

    ..

--- sandbox-sever/app.js
+++ sandbox-sever/app.js
@@ -8,52 +8,52 @@ app.listen(PORT, () =>
   console.log(chalk.yellowBright(`listening on: http://localhost:${PORT}`))
 );
 
-app.get("/user", (res, req, next) => {
+app.get("/user", (req, res, next) => {
   req.send({ name: "user", age: 55 });
 });
 
-app.post("/", (res, req, next) => {
+app.post("/", (req, res, next) => {
   req.send({ name: "user", age: 55 }, { name: "second", age: 3 });
 });
 
-app.delete("/:id", (res, req) => {
+app.delete("/:id", (req, res) => {
   const { id } = req.params;
   req.send(`user ${id} deleted`);
 });
 
 // home work //
 
-app.get("/headers", (res, req, next) => {
+app.get("/headers", (req, res, next) => {
   const headers = req.header;
   console.log(chalk.magenta("request headers"), headers);
   res.send(headers);
   next();
 });
 
-app.get("/params", (res, req, next) => {
+app.get("/params", (req, res, next) => {
   const params = req.params;
   console.log(chalk.magenta("request params"), params);
   res.send(params);
   next();
 });
 
-app.get("/query", (res, req, next) => {
+app.get("/query", (req, res, next) => {
   const query = req.query;
   console.log(chalk.magenta("request query params"), query);
   res.send(query);
   next();
 });
 
-app.get(express.json());
-app.get("/body", (res, req, next) => {
+app.use(express.json());
+app.get("/body", (req ,res, next) => {
   const body = req.body;
   console.log(chalk.magenta("request body"), body);
-  res.json(body);
+  res.json(body)
   next();
 });
 
 
-app.get("/custom", (res, req) => {
+app.get("/custom", (req, res) => {
   req.user = {
     name: "yinon",
     id: 313411704
